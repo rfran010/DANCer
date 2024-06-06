f0a0a6a0f623050b23bb55ae8f23a653f5819ab5
Oliver Tam <Oliver.Tam@nyulangone.org>
2024-06-06 14:23:05 -0400

    Added code and conda environment YAML

---
+++ DANcer/scripts/DANCer_predict.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Commented out IPython magic to ensure Python compatibility.
+## Author: Kathryn O'Neill
+## Description: A classifier on weighted gene co-expression network eigengenes
+
+# %tensorflow_version 2.x
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys, os
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+tf.keras.backend.clear_session()  # For easy reset of notebook state.
+from tensorflow import keras
+from keras import utils
+from keras import layers
+
+print("Tensorflow version: %s" % (tf.__version__))
+print("Keras version: %s" % (keras.__version__))
+print("Pandas version: %s" % (pd.__version__))
+
+if len(sys.argv) <= 2:
+    print("DANCer_predict.py [file with eigenvalues] [model weights]")
+    exit(1)
+
+# Define a classifier
+
+inputs = layers.Input(shape=(39,))
+x = layers.Dense(6,activation = 'relu')(inputs)
+outputs = layers.Dense(3,activation='softmax')(x)
+
+als_nmf_subtype_classifier = tf.keras.Model(inputs,outputs)
+
+#Predict on new data with old model but report data with more info
+
+als_nmf_subtype_classifier.load_weights(sys.argv[2])
+
+#Read input dataset and labels and convert last label column to one hot encoding
+
+input_df_new = pd.read_csv(sys.argv[1], sep = '\t', index_col = 0)
+print(input_df_new)
+
+#input_df_new = input_df_new.iloc[:,:-2]
+sample_idxs = input_df_new.index
+
+X_all = input_df_new.values
+
+softmax_output = als_nmf_subtype_classifier.predict(X_all)
+softmax_df = pd.DataFrame(softmax_output, index=sample_idxs)
+
+categorical_output = np.argmax(als_nmf_subtype_classifier.predict(X_all), axis=-1)
+categorical_df = pd.DataFrame(categorical_output, index=sample_idxs)
+
+one_outputs = [als_nmf_subtype_classifier.layers[1].output] # Extracts the outputs of the 1st layer
+activation_model =  tf.keras.Model(inputs=als_nmf_subtype_classifier.input, outputs=one_outputs) # Creates a model that will return these outputs, given the model input
+activations = activation_model.predict(X_all)
+activations_df = pd.DataFrame(activations,index = sample_idxs)
+
+X_new_pred_full = pd.concat([activations_df, softmax_df, categorical_df], axis=1)
+X_new_pred_full.columns = ["act_0", "act_1", "act_2", "act_3","act_4","act_5","soft_0","soft_1","soft_2","class"]
+
+print(X_new_pred_full)
+
+outfile = os.path.basename(sys.argv[1])
+outfile = os.path.splitext(outfile)[0] + "_classifier_out.tsv"
+X_new_pred_full.to_csv(outfile, sep='\t', header=True)
---
+++ DANcer/scripts/eigengene_calculation.R
@@ -0,0 +1,40 @@
+#!/bin/env Rscript
+
+# This is a script to preprocess vst transformed counts to eigengenes 
+
+## Check for input files. Require sample VST file and eigengene RData
+args = commandArgs(trailingOnly=T)
+if(length(args)<2){
+    stop("Usage: Rscript eigengene_calculation.R [VST file] [eigengene RData]",call.=FALSE)
+}
+
+vstfile = args[1]
+base = basename(args[1])
+base = tools::file_path_sans_ext(base)
+Rdata = args[2]
+
+## Loading Eigengene information
+library(WGCNA)
+load(file = Rdata)
+
+## Load and process VST file
+vstcounts = read.table(vstfile,header=T,row.names=1,sep="\t")
+vst_filtered = vstcounts[rownames(vstcounts) %in% names(moduleLabels),]
+
+if(dim(vst_filtered)[1] < length(moduleColors)){
+    missing_genes <-  names(moduleLabels[-which(names(moduleLabels) %in% row.names(vstcounts))])
+    print(paste(length(missing_genes), " genes are missing from the ALS subtype sc pseudobulk classifier gene model, they will be replaced with the minimum input count value. The replacement constititutes ", length(missing_genes)/length(moduleLabels)*100, "% of the total gene model. Consider this in interpretation of results.", sep=""))
+    fill <- as.data.frame(matrix(min(vstcounts),  length(missing_genes), dim(vstcounts)[2]), row.names = missing_genes)
+    colnames(fill) <- colnames(vstcounts)
+    vst_filled <- rbind(vstcounts, fill)
+    counts_module_ordered <- vst_filled[rownames(vst_filled) %in% names(moduleLabels),]
+    vst_eigen <- moduleEigengenes(t(counts_module_ordered),moduleColors, grey = FALSE)
+}else{
+    vst_eigen = moduleEigengenes(t(as.matrix(vst_filtered)),moduleColors,grey=FALSE)
+}
+
+vst_MEs = vst_eigen[["eigengenes"]]
+vst_MEs$MEviolet = NULL  # Sex specific module
+
+file = paste0(base, "_MEs.tsv")
+write.table(vst_MEs,file,sep="\t",col.names=T,row.names=T,quote=F)
